858568d7050ee60d537278546b3a76fc95276862
dawwr <dawwr@DESKTOP-9ILEQNQ>
2023-05-24 20:38:28 +0200

    Dodaj pliki projektów.

---
+++ WebApplication321/App_Start/BundleConfig.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Optimization
+
+Public Class BundleConfig
+    ' Aby uzyskać więcej informacji o grupowaniu, odwiedź stronę https://go.microsoft.com/fwlink/?LinkID=303951
+    Public Shared Sub RegisterBundles(ByVal bundles As BundleCollection)
+        bundles.Add(New ScriptBundle("~/bundles/WebFormsJs").Include(
+                        "~/Scripts/WebForms/WebForms.js",
+                        "~/Scripts/WebForms/WebUIValidation.js",
+                        "~/Scripts/WebForms/MenuStandards.js",
+                        "~/Scripts/WebForms/Focus.js",
+                        "~/Scripts/WebForms/GridView.js",
+                        "~/Scripts/WebForms/DetailsView.js",
+                        "~/Scripts/WebForms/TreeView.js",
+                        "~/Scripts/WebForms/WebParts.js"))
+
+        ' W celu zapewnienia odpowiedniego działania tych plików należy użyć odpowiedniej kolejności, ponieważ mają one jawne zależności
+        bundles.Add(New ScriptBundle("~/bundles/MsAjaxJs").Include(
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"))
+
+        ' Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
+        ' gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"))
+    End Sub
+End Class
---
+++ WebApplication321/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informacje ogólne o zestawie zależą od poniższego 
+' zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' związane z zestawem.
+
+' Przejrzyj wartości atrybutów zestawu
+<Assembly: AssemblyTitle("WebApplication321")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("WebApplication321")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów, jeśli ten projekt jest uwidaczniany w modelu COM
+<Assembly: Guid("e3eefb97-c25a-4d5b-ba94-de07c96b937c")>
+
+' Informacje o wersji zestawu obejmują następujące cztery wartości:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki, 
+' stosując znak „*”, jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
